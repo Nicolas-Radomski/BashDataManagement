87b9dddcd363b7c7e694984b1a571de6b83ba4b4
Nicolas Radomski <nicolas.radomski.phd@gmail.com>
2021-09-08 22:59:56 +0200

    Add files via upload

--- BashDataManagement.sh
+++ BashDataManagement.sh
@@ -1,11 +1,11 @@
 #### Bash-based data management ####
 
-# connect to and creat a mounting point to a personnal working area
-## connect
+# connect to a server and creat a mounting point to a personnal working area
+## connect to a server
 ssh user@??.??.??.???
-## move to personal area
+## move into the personal area
 cd /path/to/user/
-## check position of navigation
+## check the position of navigation
 pwd
 ## creat a directory dedicated to the training
 mkdir BashDataManagement
@@ -17,33 +17,33 @@ sftp://??.??.??.???/path/to/user/BashDataManagement
 cd BashDataManagement
 ## unpack the GitHub repository into a directory called repository
 git clone https://github.com/Nicolas-Radomski/BashDataManagement.git repository
-## copy the directory data inside the working area
+## copy the file input.zip from the repository
 cp repository/input.zip .
-## decompress the input.zip
+## decompress the file input.zip
 unzip input.zip
 ## remove the useless directory called repository
 rm -rf repository/
 ## remove the useless file called input.zip
 rm input.zip
-## open execution permission to the user
-chmod -R u+x input/
-## open writing permission to the user
-chmod -R u+w input/
-## open reading permission to the user
+## open the reading permission to the user
 chmod -R u+r input/
-## have a look into the input data
+## open the writing permission to the user
+chmod -R u+w input/
+## open the execution permission to the user
+chmod -R u+x input/
+## check the files available into the directory input
 ls input/
 ## creat a directory to store output data
 mkdir output
 
 # clean the file dataframe-unit1.tsv
-## check if the file is from windows or linux (if windows "\r\n" if linux "\n")
+## check if the file is from windows or linux (i.e. "\r\n" for windows or "\n" for linux)
 hexdump -c input/dataframe-unit1-transposed.tsv
 ## pass from windows to linux if necessary
 dos2unix input/dataframe-unit1-transposed.tsv
-## open the file dataframe-unit1-transposed.tsv and notice that the corresponding table is transposed
+## open the file dataframe-unit1-transposed.tsv and notice that the corresponding dataframe is transposed
 more input/dataframe-unit1-transposed.tsv
-## transpose the file dataframe-unit1-transposed.tsv, then save it in output directory as dataframe-unit1.tsv
+## transpose the file dataframe-unit1-transposed.tsv, then save it in the output directory as dataframe-unit1.tsv
 rm output/dataframe-unit1.tsv
 col="$(head -1 input/dataframe-unit1-transposed.tsv | wc -w)"
 for i in $(seq 1 $col); do
@@ -51,7 +51,7 @@ for i in $(seq 1 $col); do
 done >> output/dataframe-unit1.tsv
 ## check that the file dataframe-unit1.tsv is not transposed and notice that the sample-00016 does not have results (i.e. ?)
 more output/dataframe-unit1.tsv
-## exclude the sample without result (i.e. sample-00016) from the file dataframe-unit1.tsv, then save it in output directory as clean-dataframe-unit1.tsv
+## exclude the sample without results (i.e. sample-00016) from the file dataframe-unit1.tsv, then save it in output directory as clean-dataframe-unit1.tsv
 grep -v 'sample-00016' output/dataframe-unit1.tsv > output/clean-dataframe-unit1.tsv
 ## check if the sample-00016 does not appear in the file clean-dataframe-unit1.tsv
 more output/clean-dataframe-unit1.tsv
@@ -61,17 +61,17 @@ cat output/clean-dataframe-unit1.tsv | wc -l
 awk -F'\t' '{print NF}' output/clean-dataframe-unit1.tsv | sort -nu | tail -n 1
 
 # clean the file dataframe-unit2.tsv
-## check if the file is from windows or linux (if windows "\r\n" if linux "\n")
+## check if the file is from windows or linux (i.e. "\r\n" for windows or "\n" for linux)
 hexdump -c input/dataframe-unit2.csv
 ## pass from windows to linux if necessary
 dos2unix input/dataframe-unit2.csv
 ## open the file dataframe-unit2.csv and notice that the string 'SAMPLE' is in capital letters
 more input/dataframe-unit2.csv
-## replace the string 'sample' by 'SAMPLE' into the file dataframe-unit2.csv, then save it in output directory as clean-dataframe-unit2.csv
+## replace the string 'sample' by 'SAMPLE' into the file dataframe-unit2.csv, then save it in the output directory as clean-dataframe-unit2.csv
 sed 's@SAMPLE@sample@'g input/dataframe-unit2.csv > output/clean-dataframe-unit2.csv
 ## check if the string 'sample' is replaced by 'SAMPLE' into the file clean-dataframe-unit2.csv and notice that the samples are not sorted with comma separators (i.e. csv)
 more output/clean-dataframe-unit2.csv
-## sort per sample the file clean-dataframe-unit2.csv, replace the comma separators (i.e. csv) by tab separators (i.e. tsv), then save it in output directory as clean-dataframe-unit2.tsv
+## sort per sample the file clean-dataframe-unit2.csv, replace the comma separators (i.e. csv) by tab separators (i.e. tsv), then save it in the output directory as clean-dataframe-unit2.tsv
 sort -t ',' -k1,1 output/clean-dataframe-unit2.csv | tr ',' '\t' > output/clean-dataframe-unit2.tsv
 ## check if the samples are sorted with tab separators into the file clean-dataframe-unit2.tsv
 more output/clean-dataframe-unit2.tsv
@@ -81,27 +81,32 @@ cat output/clean-dataframe-unit2.tsv | wc -l
 awk -F'\t' '{print NF}' output/clean-dataframe-unit2.tsv | sort -nu | tail -n 1
 
 # creat and clean the file dataframe-unit3.tsv
+## check if the files are from windows or linux (i.e. "\r\n" for windows or "\n" for linux)
+hexdump -c input/sample-00001.unit3
+## pass from windows to linux if necessary
+dos2unix input/*.unit3
 ## notice that the unit3 provided a useless sample-00016.unit3 and remove it
-ll input/
+more input/sample-00016.unit3
 rm input/sample-00016.unit3
-## notice that the unit3 provided tsv files with extention .unit3, combine it, and save it in output directory as dataframe-unit3-V1.tsv
-more input/sample-00001.unit3
+## notice that the unit3 provided tsv files with extentions .unit3, combine them, and save it in the output directory as dataframe-unit3-V1.tsv
+ll input/
 grep . input/*.unit3 > output/dataframe-unit3-V1.tsv
-## notice that the colomn 1 and 2 of the file dataframe-unit3-V1.tsv are separated with double dots and replace them with tab separators
+## notice that the colomn 1 and 2 of the file dataframe-unit3-V1.tsv are separated with double dots, replace them with tab separators, then save it in the output directory as dataframe-unit3-V2.tsv
 more output/dataframe-unit3-V1.tsv
 sed 's@:@\t@'g output/dataframe-unit3-V1.tsv > output/dataframe-unit3-V2.tsv
-## notice that the the strings 'input/' and '.init3' are useless in the sample identifiers (i.e. colomn 1) and remove them
+## notice that the the strings 'input/' and '.init3' are useless in the sample identifiers (i.e. colomn 1), remove them, then save it in the output directory as dataframe-unit3-V3.tsv
 more output/dataframe-unit3-V2.tsv
 sed 's@input/@@'g output/dataframe-unit3-V2.tsv | sed 's@.unit3@@'g > output/dataframe-unit3-V3.tsv
-## notice that the file output/dataframe-unit3-V3.tsv have multiple header, then remove them, save a single header and combine both
+## notice that the file dataframe-unit3-V3.tsv have multiple header, remove them (i.e. dataframe-unit3-V4.tsv), save a single header (i.e. dataframe-unit3-V4-header.tsv) and combine both (i.e. dataframe-unit3-V5.tsv)
+### check the multiple headers of the file dataframe-unit3-V3.tsv
 more output/dataframe-unit3-V3.tsv
-### keep lines without the string '#ID'
+### keep the lines without the string '#ID' of the file dataframe-unit3-V3.tsv, then save it in the output directory as dataframe-unit3-V4.tsv
 grep -v '#ID' output/dataframe-unit3-V3.tsv > output/dataframe-unit3-V4.tsv
-### keep the first line and replace the string 'sample-00001' by 'sample'
+### keep the first line and replace the string 'sample-00001' by 'sample' of the file dataframe-unit3-V4.tsv, then save it in the output directory as dataframe-unit3-V4-header.tsv
 sed -n '1p' output/dataframe-unit3-V3.tsv | sed 's@sample-00001@sample@'g > output/dataframe-unit3-V4-header.tsv
-### combine the header and dataframe
+### combine the header (i.e. dataframe-unit3-V4-header.tsv) and dataframe (i.e. output/dataframe-unit3-V4.tsv)
 cat output/dataframe-unit3-V4-header.tsv output/dataframe-unit3-V4.tsv > output/dataframe-unit3-V5.tsv
-## identify colomns of interest: sample (i.e. colomn 1 called sample), depth of coverage (i.e. colomn 11 called Median_fold) and GC% (i.e. colomn 10 called Read_GC)
+## identify colomns of interest: sample (i.e. colomn 1 called sample), GC% (i.e. colomn 10 called Read_GC) and depth of coverage (i.e. colomn 11 called Median_fold)
 head -1 output/dataframe-unit3-V5.tsv | tr '\t' '\n' | nl
 ## extract the colomns of interest from the file dataframe-unit3-V5.tsv (i.e. 1, 10 and 11), correct fields of the header, then save it in output directory as dataframe-unit3-V1.tsv
 cut -d$'\t' -f1,10,11 output/dataframe-unit3-V5.tsv | sed 's@Read_GC@GC@'g | sed 's@Median_fold@estimated_depth@'g > output/clean-dataframe-unit3.tsv
@@ -115,7 +120,7 @@ awk -F'\t' '{print NF}' output/clean-dataframe-unit3.tsv | sort -nu | tail -n 1
 sed '1d' output/clean-dataframe-unit1.tsv > output/clean-dataframe-unit1-noheader.tsv
 sed '1d' output/clean-dataframe-unit2.tsv > output/clean-dataframe-unit2-noheader.tsv
 sed '1d' output/clean-dataframe-unit3.tsv > output/clean-dataframe-unit3-noheader.tsv
-## joint of the files clean-dataframe-unit1-noheader.tsv and clean-dataframe-unit2-noheader.tsv
+## joint the files clean-dataframe-unit1-noheader.tsv and clean-dataframe-unit2-noheader.tsv
 join -t $'\t' -1 1 -2 1 output/clean-dataframe-unit1-noheader.tsv output/clean-dataframe-unit2-noheader.tsv -a 2 > output/clean-dataframe-unit1-unit2-noheader.tsv
 ## joint of the files output/clean-dataframe-unit1-unit2-noheader.tsv and clean-dataframe-unit3-noheader.tsv
 join -t $'\t' -1 1 -2 1 output/clean-dataframe-unit1-unit2-noheader.tsv output/clean-dataframe-unit3-noheader.tsv -a 2 > output/clean-dataframe-unit1-unit2-unit3-noheader.tsv
@@ -131,3 +136,5 @@ more output/clean-dataframe.tsv
 cat output/clean-dataframe.tsv | wc -l
 ## check the number of colomns of the file clean-dataframe.tsv (i.e. 5 colomns)
 awk -F'\t' '{print NF}' output/clean-dataframe.tsv | sort -nu | tail -n 1
+## save the file clean-dataframe.tsvin your computeur
+scp output/clean-dataframe.tsv user@???.???.?.??:/path/to/RstudioWorkingDirectory/BashDataManagement/.
